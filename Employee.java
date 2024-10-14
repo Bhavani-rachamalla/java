518c5220dda109f972a3eebf14cf0bfec07902a7
Bhavani-rachamalla <151708866+Bhavani-rachamalla@users.noreply.github.com>
2024-10-14 12:49:57 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Employee
+{
+    float salary=500;
+}
+class Programmer extends Employee
+{
+    int bonus;
+    void display()
+    {
+        //system.out.println("Salary of the employee is",+(salary+Bonus));
+    }
+    public static void main(String args[])
+    {
+        Employee e=new Employee();
+        Programmer p=new Programmer();
+        e.salary=500;
+        p.bonus=150;
+        System.out.println("salary of the employee is"+e.salary);
+        System.out.println("Bonus for the programmer is"+p.bonus);
+    }
+}
